8a570cef8bd3cd55794c28e0632673a2d09c4683
ArthurK <arthurromain.kiehl@gmail.com>
2024-02-02 17:08:20 +0100

    update model.pkl

--- vs_code_api.py
+++ vs_code_api.py
@@ -19,10 +19,13 @@ from fastapi.responses import JSONResponse
 
 
 # Charger le modèle MLFlow
-model_path = r"mlruns\159852288404653738\89e2dedfd3dd428b849adecd8c60de14\artifacts\model_lgbm_class_weight_best_model"
+
+# model_path = r"mlruns\159852288404653738\89e2dedfd3dd428b849adecd8c60de14\artifacts\model_lgbm_class_weight_best_model"
 # model_path = r"mlruns\159852288404653738\89e2dedfd3dd428b849adecd8c60de14\artifacts\model_lgbm_class_weight_best_model"
 # model = mlflow.sklearn.load_model(model_path)
-model = pd.read_pickle(model_path +r"\model.pkl")
+# model = pd.read_pickle(model_path +r"\model.pkl")
+
+model = pd.read_pickle('model.pkl')
 df = pd.read_pickle('test_samp.pkl')
 
 # débug
